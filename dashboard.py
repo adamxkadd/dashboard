7a2e6c02c6c67ee51c21600b585df1b48e4f7a74
adamxkadd <143011477+adamxkadd@users.noreply.github.com>
2023-09-03 16:08:18 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,12 +1,8 @@
 import streamlit as st
 import requests
 
-# Interface Streamlit
 st.title("Application Streamlit avec API FastAPI")
 
 if st.button("Appeler l'API"):
-    # Fait une requête HTTP à l'API FastAPI pour obtenir une réponse
-    response = requests.get("https://api-mu-nine.vercel.app/")  # Assurez-vous de l'URL correcte pour votre API FastAPI
-    
-    # Affiche la réponse de l'API dans Streamlit
+    response = requests.get("https://api-mu-nine.vercel.app/") 
     st.write("Réponse de l'API:", response.json())
