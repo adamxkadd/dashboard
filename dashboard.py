0a51ecf1c23c0f35eb622f291ea7c480f5dcc0ed
adamxkadd <143011477+adamxkadd@users.noreply.github.com>
2023-09-03 22:49:30 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -3,7 +3,10 @@ import streamlit as st
 
 st.title("Dashboard Scoring Bank")
 
-if st.button("Obtenir les données de l'API"):
+if st.button("Obtenir les données de l API"):
     response = requests.get("https://scoring-bank.vercel.app/").json()
     st.write("Données de l'API :")
     st.write(response)
+
+    st.write("Élément 'text' de l'API :")
+    st.write(response['text'])
