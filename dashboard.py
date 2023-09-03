0ef43a50a3de2755cd9b3b535587c6793ea63641
adamxkadd <143011477+adamxkadd@users.noreply.github.com>
2023-09-03 16:11:40 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,8 +1,27 @@
-import streamlit as st
 import requests
 
-st.title("Application Streamlit avec API FastAPI")
+# URL de l'API que vous souhaitez appeler
+url = "https://api-mu-nine.vercel.app/donnees"
 
-if st.button("Appeler l'API"):
-    response = requests.get("https://api-mu-nine.vercel.app/") 
-    st.write("Réponse de l'API:", response.json())
+# Effectuer la requête GET à l'API
+response = requests.get(url)
+
+# Vérifier si la requête a réussi (code de statut 200)
+if response.status_code == 200:
+    # Récupérer les données JSON de la réponse
+    donnees = response.json()
+    print("Données de l'API:")
+    print(donnees)
+else:
+    print("La requête à l'API a échoué avec le code de statut:", response.status_code)
+
+
+
+# import streamlit as st
+# import requests
+
+# st.title("Application Streamlit avec API FastAPI")
+
+# if st.button("Appeler l'API"):
+#     response = requests.get("https://api-mu-nine.vercel.app/") 
+#     st.write("Réponse de l'API:", response.json())
